dac7e8ac6ca233009e7b5fd1ddde1915f30ed13b
Jordan Worthington <31937407+jw019211@users.noreply.github.com>
2017-10-12 11:34:20 -0400

    Update course.h

--- course.h
+++ course.h
@@ -7,6 +7,12 @@
 		John Dolan	School of EECS		Summer 2013
 ************************************************************************/
 
+/**
+*	@file course.h
+*	@author John Dolan
+*	@date Summer 2013
+*/
+
 #include<iostream>
 #include<string>
 #ifndef COURSE_H
